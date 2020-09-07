80a56fb4cf27f71344ce191d4e3dcdd921500a12
ArmandoS <jsandino@aimservices.tech>
2020-09-07 23:48:04 +0200

    feat; agregar firebase para iniciar session con Google

--- tienda/applications/users/views.py
+++ tienda/applications/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+#
+from django.views.generic import TemplateView
 
-# Create your views here.
+class LoginUser(TemplateView):
+
+    template_name = 'users/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(LoginUser, self).get_context_data(**kwargs)
+        return context
+    
