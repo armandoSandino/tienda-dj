8c689cf26b2ebf7345481b8acffbf4e8c65e3798
ArmandoS <jsandino@aimservices.tech>
2020-09-07 22:12:29 +0200

    chore; agregar configuracion de mysql

--- tienda/tienda/settings/local.py
+++ tienda/tienda/settings/local.py
@@ -7,6 +7,20 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -17,6 +31,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
