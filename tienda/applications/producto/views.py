c1751d3e2f03facf12115332e89182e63f7ac478
ArmandoS <jsandino@aimservices.tech>
2020-09-08 16:51:45 +0200

    chore; implemetar , authentication_classes y permission_classes, para proteger  ruta de listar producto por usuario

--- tienda/applications/producto/views.py
+++ tienda/applications/producto/views.py
@@ -2,6 +2,9 @@
 from rest_framework.generics import (
     ListAPIView
 )
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from django.shortcuts import render
 # serializers
 from .serializers import ProductSerializer
@@ -13,7 +16,17 @@ class ListProductUser(ListAPIView):
 
     # definir el serializador
     serializer_class = ProductSerializer
+    # definir el tipo de autenticacion para poder implementar este recurso
+    # Decifrara el token e identificara/autenticara al usuario
+    authentication_classes = (TokenAuthentication,)
+
+    # Definir los tipos de permisos tiene habilitados esta vista
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        
+        # obtener usuario
+        print(self.request.user)
+        el_usuario = self.request.user
 
-        return Product.objects.all()
+        return Product.objects.productos_por_usuario(el_usuario)
